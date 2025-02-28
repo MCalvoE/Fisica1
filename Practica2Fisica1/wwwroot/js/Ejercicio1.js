ac92f6647d379425c9343d22a0a8dc423caaa374
Matias <matiascalvoecheverria@gmail.com>
2025-02-28 02:13:14 -0600

    Entrega práctica 2 física 1

---
+++ Practica2Fisica1/wwwroot/js/Ejercicio1.js
@@ -0,0 +1,15 @@
+﻿function calcularTiempo() {
+
+    var altura = parseFloat(document.getElementById("altura").value);
+
+    var velocidad = 100;
+
+    if (isNaN(altura) || altura <= 0) {
+        document.getElementById("resultado").innerText = "Por favor, ingrese una estatura válida.";
+        return;
+    }
+
+    var tiempo = altura / velocidad;
+
+    document.getElementById("resultado").innerText = "El tiempo estimado es: " + tiempo.toFixed(5) + " segundos.";
+}
